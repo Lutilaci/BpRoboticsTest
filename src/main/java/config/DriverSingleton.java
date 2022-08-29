4f8bbc401c23254fd0c144b86ec87d2f69e8c840
Lutilaci <lutilaci@gmail.com>
2022-08-29 12:16:50 +0200

    add DriverSingleton

---
+++ src/main/java/config/DriverSingleton.java
@@ -0,0 +1,24 @@
+package config;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// Can I have a review pls?
+
+public class DriverSingleton {
+
+    private static WebDriver driver = null;
+
+    private DriverSingleton() {}
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+    public static void quit(){
+        driver.quit();
+        driver = null;
+    }
+}
